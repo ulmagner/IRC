992ce3b2536121db2d34536d8257cf2ce9e55555
Ulysse Magneron <ulmagner@f2r11s3.clusters.42paris.fr>
2025-07-17 12:29:00 +0200

    [MOD] simple connection ---> multiple connection using epoll

--- includes/Serv.hpp
+++ includes/Serv.hpp
@@ -6,24 +6,29 @@
 /*   By: ulmagner <ulmagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:55:09 by ulmagner          #+#    #+#             */
-/*   Updated: 2025/07/15 15:04:26 by ulmagner         ###   ########.fr       */
+/*   Updated: 2025/07/17 12:21:35 by ulmagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef Serv_hpp
 #define Serv_hpp
+#define MAX_EVENTS 1024
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <exception>
 #include <deque>
+#include <vector>
 #include <list>
 #include <algorithm>
 #include <ctime>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
 
 class Serv {
     private:
@@ -31,8 +36,10 @@ class Serv {
         std::string _pass;
         int _socketfd;
         int _epollfd;
+        std::vector<int> _connections;
         int isValidPort( const std::string& port ) const;
         void isValidPass( const std::string& pass ) const;
+        void createTcpServerSocket( void );
     public:
         Serv( char **arg );
         ~Serv( void );
--- srcs/Serv.cpp
+++ srcs/Serv.cpp
@@ -6,13 +6,13 @@
 /*   By: ulmagner <ulmagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:58:33 by ulmagner          #+#    #+#             */
-/*   Updated: 2025/07/15 15:25:14 by ulmagner         ###   ########.fr       */
+/*   Updated: 2025/07/17 12:21:39 by ulmagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serv.hpp"
 
-Serv::Serv( char **arg ) : _socketfd(0), _epollfd(-1) {
+Serv::Serv( char **arg ) : _socketfd(0), _epollfd(0) {
     this->_port = this->isValidPort(arg[1]);
     this->isValidPass(arg[2]);
     this->_pass = arg[2];
@@ -20,22 +20,69 @@ Serv::Serv( char **arg ) : _socketfd(0), _epollfd(-1) {
 
 Serv::~Serv( void ) {}
 
-void Serv::run( void ) {
+void Serv::createTcpServerSocket( void ) {
     this->_socketfd = socket(AF_INET, SOCK_STREAM, this->_epollfd);
 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(this->_port);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    
+
     bind(this->_socketfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
     listen(this->_socketfd, 10);
+}
 
-    int clientSocket = accept(this->_socketfd, nullptr, nullptr);
+void Serv::run( void ) {
+    createTcpServerSocket();
+    fcntl(this->_socketfd, F_SETFL, O_NONBLOCK);
+    struct epoll_event ev, events[MAX_EVENTS];
+    this->_connections.push_back(this->_socketfd);
+    int nfds, clientSocket;
+    this->_epollfd = epoll_create1(0);
+    if (this->_epollfd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+    ev.events = EPOLLIN;
+    ev.data.fd = this->_socketfd;
+    if (epoll_ctl(this->_epollfd, EPOLL_CTL_ADD, this->_socketfd, &ev) == -1) {
+        perror("epoll_ctl: this->_socketfd");
+        exit(EXIT_FAILURE);
+    }
+    for (;;) {
+        nfds = epoll_wait(this->_epollfd, events, MAX_EVENTS, -1);
+        if (nfds == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
 
-    
-    
+        for (int n = 0; n < nfds; ++n) {
+            if (events[n].data.fd == this->_socketfd) {
+                sockaddr_in clientAddr;
+                socklen_t addrlen = sizeof(clientAddr);
+                clientSocket = accept(events[n].data.fd, (struct sockaddr*)&clientAddr, &addrlen);
+                if (clientSocket == -1) {
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+                fcntl(clientSocket, F_SETFL, O_NONBLOCK);
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = clientSocket;
+                this->_connections.push_back(ev.data.fd);
+                if (epoll_ctl(this->_epollfd, EPOLL_CTL_ADD, events[n].data.fd, &ev) == -1) {
+                    perror("epoll_ctl: clientSocket");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else {
+                char buffer[MAX_EVENTS] = {0};
+                recv(events[n].data.fd, buffer, sizeof(buffer), 0);
+                std::cout << "Message from client: " << buffer << std::endl;
+            }
+        }
+    }
+    close(this->_socketfd);
 }
 
 int Serv::isValidPort( const std::string& port ) const {
