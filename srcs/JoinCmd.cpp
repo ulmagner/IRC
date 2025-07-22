ee2998eb6822f9749f8dee311414bc21932ac9c2
Ulysse Magneron <ulmagner@f6r6s7.clusters.42paris.fr>
2025-07-22 17:20:20 +0200

    [ADD] nope

--- srcs/JoinCmd.cpp
+++ srcs/JoinCmd.cpp
@@ -6,7 +6,7 @@
 /*   By: ulmagner <ulmagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 10:17:41 by ulmagner          #+#    #+#             */
-/*   Updated: 2025/07/22 16:47:47 by ulmagner         ###   ########.fr       */
+/*   Updated: 2025/07/22 17:10:24 by ulmagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "Serv.hpp"
 
 JoinCmd::JoinCmd( std::vector<std::string> tokens, Serv& serv ) : ACmd(tokens[0]), _tokens(tokens), _serv(serv) {
-	if (tokens.size() < 2) {
+	if (tokens.size() < 1 || tokens.size() > 2) {
 		throw JoinCmd::FormatException();
 	}
 }
@@ -26,14 +26,16 @@ void JoinCmd::executeCmd( Client& client ) {
 	for (size_t i = 0; i < this->_tokens.size(); ++i) {
 		std::cout << this->_tokens[i] << std::endl;
 	}
-	// std::vector<Channel>::const_iterator it = this->_serv.getChannels().begin();
-	// for (;it != this->_serv.getChannels().end(); ++it) {
-	// 	for (size_t i = 0; i < this->_serv.getChannels().size(); ++i) {
-	// 		if (it->getName() == this->_tokens[1]) {
-	// 			;
-	// 		}
-	// 	}
-	// }
+	std::vector<std::string> chan;
+	std::vector<std::string> key;
+	std::vector<Channel>::const_iterator it = this->_serv.getChannels().begin();
+	for (;it != this->_serv.getChannels().end(); ++it) {
+		for (size_t i = 0; i < this->_serv.getChannels().size(); ++i) {
+			if (it->getName() == this->_tokens[1]) {
+				;
+			}
+		}
+	}
 }
 
 const char* JoinCmd::FormatException::what() const throw()
